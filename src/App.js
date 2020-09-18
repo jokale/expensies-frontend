b91d33a52330dae957f7be10967bb2b5f8360a7f
jokale <kalemajoanna@gmail.com>
2020-09-18 13:28:49 +0100

    change app.js into class and import navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { render } from '@testing-library/react';
 
-function App() {
+class App  extends React.Component {
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      
+       <Navbar/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    
     </div>
   );
 }
-
+}
 export default App;
