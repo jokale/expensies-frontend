f6e528a9b5fd27e9147737dfbd22a25dcd1ee990
jokale <kalemajoanna@gmail.com>
2020-09-19 13:50:49 +0100

    fix form

--- src/Components/NewExpense.js
+++ src/Components/NewExpense.js
@@ -9,14 +9,17 @@ class NewExpense extends React.Component  {
          <h2>Create a new expense</h2>
          <form>
 
-        <br></br> <label>
-                Name:
-                <br></br> <input type="text" name="name" /> <br></br>
-                <br></br></label>  <br></br>
+                 <br></br> <label>Title</label><br></br>
+                <br></br> <input type="text" name="name" placeholder="Burgers with Sally" /> <br></br>
                 <br></br> <label for="expdate">Date:</label>  <br></br>
-                <br></br><input type="datetime-local" id="expense-date" name="expense-date"></input> <br></br>
-                <br></br><label> detaiils</label>  <br></br>
-                <br></br><textarea id="details-expense" name="d-expense"></textarea>  <br></br>
+                <br></br><input type="datetime-local" id="expense-date" name="expense-date" placeholder="00/00/00 at 00:00"></input> <br></br>
+                <br></br><label> Details</label>  <br></br>
+                <br></br><textarea id="details-expense" name="d-expense" placeholder="I had a burger at ..."></textarea>  <br></br>
+                <br></br><label> Amount (in GBP)</label>  <br></br>
+
+                <br></br> <input type="number" name="amount"  placeholder="£00000"/>  <br></br>
+
+                
                 <br></br><input type="submit" value="Submit" /> <br></br>
          
          </form>
